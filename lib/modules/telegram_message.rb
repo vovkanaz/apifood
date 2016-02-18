771576574c8853b06a4a7370855a11b2be236d84
Patriarh22 <kvs2204@gmail.com>
2016-02-18 15:47:17 +0200

    Modified method params_for_order

--- lib/modules/telegram_message.rb
+++ lib/modules/telegram_message.rb
@@ -4,7 +4,7 @@ require 'yaml'
 class TelegramMessageService
   include Singleton
 
-  TG_CONFIG = YAML.load_file(File.expand_path('/home/intel/apifood/config/telegram.yml', __FILE__))
+  TG_CONFIG = YAML.load_file(File.expand_path('/home/kvs/Ruby/apifood/config/telegram.yml', __FILE__))
 
   def send(message)
     system("#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e 'msg #{TG_CONFIG['peer']} #{message}'")
