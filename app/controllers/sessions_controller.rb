1aff3c85c06eaa1d5f7eda04168f6567a111db7a
vovkanaz <vovkanaz@gmail.com>
2016-03-12 19:26:57 +0200

    Added bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:create]
+
   def create
     @auth = request.env["omniauth.auth"]
     @token = @auth["credentials"]["token"]
@@ -17,12 +18,19 @@ class SessionsController < ApplicationController
 
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    @events = result.data['items']
-    redirect_to root_path
+
+    if result.data.items.empty?
+      redirect_to root_path
+      flash[:alert] = "No events find"
+    else
+      flash[:notice] = "Events successfully obtained"
+      @events = result.data['items']
+    end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
+
 end
